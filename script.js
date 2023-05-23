3eb5c30c306f8081ffebdf84d73847ec62600658
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-23 15:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function deleteElement(){
+	let selectElement = document.getElementsByTagName("select")
+	let selectedElement = selectElement.value;
+	let childrenList = selectElement.children;
+
+	for(let i = 0; i < childrenList.length; i++){
+		if(selectedElement === childrenList[i].value){
+			childrenList[i].remove();
+		}
+	}
+}
